ebee3f05b057ccd7e5df0913401ad8fd6d446f77
poratox78-spec <poratox78@gmail.com>
2026-01-11 11:27:48 +0100

    Update world.py

--- simulation/world.py
+++ simulation/world.py
@@ -7,3 +7,14 @@ class World:
     def signal(self):
         return random.uniform(-1, 1) + self.bias
         
+class World:
+    def __init__(self, bias=0.0):
+        self.bias = bias
+        self.pressure = 0.0
+
+    def signal(self):
+        return random.uniform(-1, 1) + self.bias - self.pressure
+
+    def update(self, population):
+        self.pressure = population.average_fitness() * 0.01
+        
