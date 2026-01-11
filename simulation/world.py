d7e8915c0fa1844d3661fd4029f59782e73967ac
poratox78-spec <poratox78@gmail.com>
2026-01-11 10:09:07 +0100

    Update world.py

--- simulation/world.py
+++ simulation/world.py
@@ -1,6 +1,9 @@
 import random
 
 class World:
+    def __init__(self, bias=0.0):
+        self.bias = bias
+
     def signal(self):
-        return random.uniform(-1, 1)
-      
+        return random.uniform(-1, 1) + self.bias
+        
